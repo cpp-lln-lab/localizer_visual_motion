7c46d4971ceb88bdbe0ff39fab31f2fb509d4e65
Remi Gau <remi_gau@hotmail.com>
2020-04-01 18:55:48 +0200

    use pascalCase inside setParameters

--- Visual-loc_translational/setParameters.m
+++ Visual-loc_translational/setParameters.m
@@ -1,23 +1,23 @@
-function [ExpParameters, Cfg] = setParameters
+function [expParameters, cfg] = setParameters
 
 ExpParameters = struct; % Initialize the parameters variables
 Cfg           = struct; % Initialize the general configuration variables
 
-ExpParameters.task = 'VisualLoc';
+expParameters.task = 'VisualLoc';
 
 
 %% Debug mode settings
-Cfg.debug               = true;  % To test the script out of the scanner, skip PTB sync
-Cfg.testingSmallScreen  = false; % To test on a part of the screen, change to 1
-Cfg.testingTranspScreen = true;  % To test with trasparent full size screen 
-Cfg.stimPosition        = 'PC';  % 'Scanner': means that it removes the lower 1/3 of the screen (the coil hides the lower part of the screen)
+cfg.debug               = true;  % To test the script out of the scanner, skip PTB sync
+cfg.testingSmallScreen  = false; % To test on a part of the screen, change to 1
+cfg.testingTranspScreen = true;  % To test with trasparent full size screen
+cfg.stimPosition        = 'PC';  % 'Scanner': means that it removes the lower 1/3 of the screen (the coil hides the lower part of the screen)
 
 
 %% MRI settings
-Cfg.device        = 'PC';  % 'PC': does not care about trigger - otherwise use 'Scanner'
-Cfg.triggerKey    = 't';   % Set the letter sent by the trigger to sync stimulation and volume acquisition
-Cfg.numTriggers   = 4;     
-Cfg.eyeTracker    = false; % Set to 'true' if you are testing in MRI and want to record ET data
+cfg.device        = 'PC';  % 'PC': does not care about trigger - otherwise use 'Scanner'
+cfg.triggerKey    = 't';   % Set the letter sent by the trigger to sync stimulation and volume acquisition
+cfg.numTriggers   = 4;
+cfg.eyeTracker    = false; % Set to 'true' if you are testing in MRI and want to record ET data
 
 
 %% Keyboards
@@ -43,16 +43,16 @@ Cfg.screenDistance    = 134; % Distance from the screen in cm
 Cfg.diameterAperture  = 8;   % Diameter/length of side of aperture in Visual angles
 
 % Monitor parameters for PTB
-Cfg.screen           = max(Screen('Screens')); % Main screen
-Cfg.white            = [255 255 255];
-Cfg.black            = [ 0   0   0 ];
-Cfg.red              = [255  0   0 ];
-Cfg.grey             = mean([Cfg.black; Cfg.white]);
-Cfg.backgroundColor  = Cfg.black;
-Cfg.textColor        = Cfg.white;
-Cfg.textFont         = 'Courier New';
-Cfg.textSize         = 18;
-Cfg.textStyle        = 1;
+cfg.screen           = max(Screen('Screens')); % Main screen
+cfg.white            = [255 255 255];
+cfg.black            = [ 0   0   0 ];
+cfg.red              = [255  0   0 ];
+cfg.grey             = mean([cfg.black; cfg.white]);
+cfg.backgroundColor  = cfg.black;
+cfg.textColor        = cfg.white;
+cfg.textFont         = 'Courier New';
+cfg.textSize         = 18;
+cfg.textStyle        = 1;
 
 % Keyboard
 Cfg.escapeKey        = 'Escape';
@@ -101,41 +101,41 @@ switch Cfg.device
 end
 
 %% Experiment Design
-ExpParameters.names              = {'static','motion'};
-ExpParameters.possibleDirections = [-1 1]; % 1 motion , -1 static
-ExpParameters.numBlocks          = size(ExpParameters.possibleDirections,2);
-ExpParameters.numRepetitions     = 1;      %AT THE MOMENT IT IS NOT SET IN THE MAIN SCRIPT
-ExpParameters.IBI                = 0; %8;      
-ExpParameters.ISI                = 0.1;    % Time between events in secs
-ExpParameters.onsetDelay         = 5;      % Number of seconds before the motion stimuli are presented
-ExpParameters.endDelay           = 1;      % Number of seconds after the end all the stimuli before ending the run
+expParameters.names              = {'static','motion'};
+expParameters.possibleDirections = [-1 1]; % 1 motion , -1 static
+expParameters.numBlocks          = size(expParameters.possibleDirections,2);
+expParameters.numRepetitions     = 1;      %AT THE MOMENT IT IS NOT SET IN THE MAIN SCRIPT
+expParameters.IBI                = 0; %8;
+expParameters.ISI                = 0.1;    % Time between events in secs
+expParameters.onsetDelay         = 5;      % Number of seconds before the motion stimuli are presented
+expParameters.endDelay           = 1;      % Number of seconds after the end all the stimuli before ending the run
 
 
 %% Visual Stimulation
-ExpParameters.experimentType    = 'Dots';  % Visual modality is in RDKs %NOT USED IN THE MAIN SCIPT
-ExpParameters.speedEvent        = 8;       % speed in visual angles
-ExpParameters.numEventsPerBlock = 12;      % Number of events per block (should not be changed)
-ExpParameters.eventDuration     = .9;
-ExpParameters.coh               = 1;       % Coherence Level (0-1)
-ExpParameters.maxDotsPerFrame   = 300;     % Maximum number dots per frame (Number must be divisible by 3)
-ExpParameters.dotLifeTime       = 0.2;     % Dot life time in seconds
-ExpParameters.dontClear         = 0;
-ExpParameters.dotSize           = 0.1;     % Dot Size (dot width) in visual angles.
-ExpParameters.dotColor          = Cfg.white;
+expParameters.experimentType    = 'Dots';  % Visual modality is in RDKs %NOT USED IN THE MAIN SCIPT
+expParameters.speedEvent        = 8;       % speed in visual angles
+expParameters.numEventsPerBlock = 12;      % Number of events per block (should not be changed)
+expParameters.eventDuration     = 5;
+expParameters.coh               = 1;       % Coherence Level (0-1)
+expParameters.maxDotsPerFrame   = 300;     % Maximum number dots per frame (Number must be divisible by 3)
+expParameters.dotLifeTime       = 1;     % Dot life time in seconds
+expParameters.dontClear         = 0;
+expParameters.dotSize           = 0.1;     % Dot Size (dot width) in visual angles.
+expParameters.dotColor          = cfg.white;
 
 
 %% Task(s)
 
 % Instruction
-ExpParameters.TaskInstruction = '1-Detect the RED fixation cross\n \n\n';
+expParameters.TaskInstruction = '1-Detect the RED fixation cross\n \n\n';
 
 ExpParameters.responseKey = {'space'};
 
 
 %% Task 1 - Fixation cross
-ExpParameters.Task1 = true; % true / false
+expParameters.Task1 = true; % true / false
 
-if ExpParameters.Task1
+if expParameters.Task1
     % Used Pixels here since it really small and can be adjusted during the experiment
     ExpParameters.fixCrossDimPix               = 10;   % Set the length of the lines (in Pixels) of the fixation cross
     ExpParameters.lineWidthPix                 = 4;    % Set the line width (in Pixels) for our fixation cross
@@ -154,7 +154,9 @@ if mod(ExpParameters.maxDotsPerFrame,3) ~= 0
     error('Number of dots should be divisible by 3.')
 end
 
-if Cfg.debug
+
+
+if cfg.debug
     fprintf('\n\n\n\n')
     fprintf('######################################## \n')
     fprintf('##  DEBUG MODE, NOT THE SCANNER CODE  ## \n')
