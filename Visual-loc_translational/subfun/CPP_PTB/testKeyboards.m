ce2fa5b348880d7e0bd58579766d68eb1b62faef
Remi Gau <remi_gau@hotmail.com>
2020-04-02 16:27:35 +0200

    make testKeyboards more explicit

--- Visual-loc_translational/subfun/CPP_PTB/testKeyboards.m
+++ Visual-loc_translational/subfun/CPP_PTB/testKeyboards.m
@@ -2,12 +2,12 @@ function testKeyboards(cfg)
 
 % Computer keyboard to quit if it is necessary
 % cfg.keyboard
-fprintf('\n This is a test: press any key to on keyboard\n');
+fprintf('\n This is a test: press any key to on the experimenter keyboard\n');
 KbPressWait(cfg.keyboard);
 
 % For key presses for the subject
 % cfg.responseBox
-fprintf('\n This is a test: press any key to on the response box\n');
+fprintf('\n This is a test: press any key to on the participant response box\n');
 KbPressWait(cfg.responseBox);
 
 
