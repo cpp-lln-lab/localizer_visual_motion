df8fa69fe8000df46cf928777133276aafdf5a2c
Remi Gau <remi_gau@hotmail.com>
2020-04-03 20:00:24 +0200

    add a time out to test keyboard

--- Visual-loc_translational/subfun/CPP_PTB/testKeyboards.m
+++ Visual-loc_translational/subfun/CPP_PTB/testKeyboards.m
@@ -1,14 +1,49 @@
 function testKeyboards(cfg)
 
-% Computer keyboard to quit if it is necessary
+timeOut = 5;
+
+
+
+% Main keyboard used by the experimenter to quit the experiment if it is necessary
 % cfg.keyboard
 fprintf('\n This is a test: press any key to on the experimenter keyboard\n');
-KbPressWait(cfg.keyboard);
+t = GetSecs;
+[~, keyCode, ~] = KbPressWait(cfg.keyboard, t+timeOut);
+throwError(keyCode, cfg.keyboard)
+
 
 % For key presses for the subject
 % cfg.responseBox
 fprintf('\n This is a test: press any key to on the participant response box\n');
-KbPressWait(cfg.responseBox);
+t = GetSecs;
+[~, keyCode, ~] = KbPressWait(cfg.responseBox, t+timeOut);
+throwError(keyCode)
+
+end
+
+
+function throwError(keyCode, deviceNumber)
+
+text1 = '\nYou asked for this keyboard device number to be used as experimenter keyboard.\n\n';
+
+text2 = '\nThese are the devices currently connected.\n\n';
+
+errorText = 'No key was pressed. Did you configure the keyboards properly? See message above for more info.';
+
+
+if all(keyCode==0)
+    
+    fprintf(text1);
+    
+    disp(deviceNumber)
+    
+    fprintf(text2);
+    
+    [keyboardNumbers, keyboardNames] = GetKeyboardIndices  %#ok<*NOPRT,*ASGLU>
+    
+    error(errorText)
+    
+end
 
 
 end
