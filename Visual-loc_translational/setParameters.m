d85d97562ed539ff65eb5d65594311f25daa9939
Remi Gau <remi_gau@hotmail.com>
2020-04-14 12:17:56 +0200

    add function to test content cfg

--- Visual-loc_translational/setParameters.m
+++ Visual-loc_translational/setParameters.m
@@ -155,32 +155,8 @@ if expParameters.Task1
 end
 
 
-
-%% Checking some inputs
-
-
-
-%% Setting some defaults: no need to change things here
-if ~isfield(expParameters, 'outputDir')
-    expParameters.dataDir = fullfile(...
-        fileparts(mfilename('fullpath')), ...
-        'output');
-end
-
-% set empty values for a series of field if they have not been specified
-fields2Check = { ...
-    'ce', ...
-    'dir', ...  % For BIDS file naming: phase encoding direction of acquisition for fMRI
-    'rec', ...  % For BIDS file naming: reconstruction of fMRI images
-    'echo', ... % For BIDS file naming: echo fMRI images
-    'acq'       % For BIDS file naming: acquisition of fMRI images
-    };
-for iField = 1:numel(fields2Check)
-    if ~isfield(expParameters, fields2Check{iField})
-        expParameters = setfield(expParameters, fields2Check{iField}, []); %#ok<SFLD>
-    end
-end
-
+% Setting some defaults: no need to change things here
+expParameters = checkCFG(expParameters);
 
 
 if cfg.debug
