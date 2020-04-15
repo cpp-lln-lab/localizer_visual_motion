4233d51f7a5c4a3dcd3d10cea527785f2490fb7e
Remi Gau <remi_gau@hotmail.com>
2020-04-15 17:55:19 +0200

    fix saveEventsFile after implementing unit test

--- Visual-loc_translational/subfun/CPP_PTB/saveEventsFile.m
+++ Visual-loc_translational/subfun/CPP_PTB/saveEventsFile.m
@@ -1,11 +1,9 @@
-function [ logFile ] = saveBehOutput(logFile, expParameters, input, varargin)
+function [ logFile ] = saveEventsFile(input, expParameters, logFile, varargin)
 
-
-'BlockNumber', ...
-    'EventNumber', ...
-    'Direction', ...
-    'IsFixationTarget', ...
-
+if nargin<3 || isempty(logFile)
+    logFile = struct();
+end
+    
 switch input
     
     case 'open'
@@ -17,19 +15,19 @@ switch input
             fullfile(expParameters.outputDir, expParameters.fileName.events), ...
             'w');
         
-        logFile.onset = [];
-        logFile.trial_type = [];
-        logFile.duration = [];
+        logFile.onset = {};
+        logFile.trial_type = {};
+        logFile.duration = {};
         
         % print the basic BIDS columns
-        fprintf(eventLogFile, '%s\t%s\t%s\t', 'onset', 'trial_type', 'duration');
+        fprintf(logFile.eventLogFile, '%s\t%s\t%s\t', 'onset', 'trial_type', 'duration');
         
         % print any extra column specified by the user
         % also prepare an empty field in the structure to collect data
         % for those
         for iExtraColumn = 1:numel(varargin)
             fprintf(logFile.eventLogFile,'%s\t', lower(varargin{iExtraColumn}));
-            logFile = setfield(logFile, lower(varargin{iExtraColumn}));
+            logFile = setfield(logFile, lower(varargin{iExtraColumn}), []);
         end
         
         % next line so we start printing at the right place
@@ -40,20 +38,28 @@ switch input
         
         % appends to the logfile all the data stored in the structure 
         % first with the standard BIDS data and then any extra things
-        for iEvent = 1:size(onset,1)
+        for iEvent = 1:size(logFile.onset,1)
             
-            fprintf(logFile.eventLogFile,'%f\%s\t%f\t',...
-                logFile.onset(iEvent,:), ...
-                logFile.trial_type(iEvent,:), ...
-                logFile.duration(iEvent,:));
+            fprintf(logFile.eventLogFile,'%f\t%s\t%f\t',...
+                logFile.onset{iEvent}, ...
+                logFile.trial_type{iEvent}, ...
+                logFile.duration{iEvent});
             
             for iExtraColumn = 1:numel(varargin)
+                
                 data = getfield(logFile, lower(varargin{iExtraColumn}));
-                if class(data) == 'char'
+                data = data{iEvent};
+                
+                if isempty(data)
+                    data = NaN;
+                end
+                     
+                if ischar(data)
                     fprintf(logFile.eventLogFile, '%s\t', data);
                 else
                     fprintf(logFile.eventLogFile, '%f\t', data);
                 end
+                
             end
             
             fprintf(logFile.eventLogFile, '\n');
