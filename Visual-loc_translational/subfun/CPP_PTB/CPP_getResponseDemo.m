17a1bfbc0fb3f6f0405235509c10a8174f39ef2f
Remi Gau <remi_gau@hotmail.com>
2020-04-21 17:17:11 +0200

    update getResponse demo

--- Visual-loc_translational/subfun/CPP_PTB/CPP_getResponseDemo.m
+++ Visual-loc_translational/subfun/CPP_PTB/CPP_getResponseDemo.m
@@ -93,6 +93,9 @@ WaitSecs(5);
 % Check what keys were pressed (all of them)
 responseEvents = getResponse('check', cfg, expParameters, 0, 1);
 
+% The following line would only return key presses and not releases
+% responseEvents = getResponse('check', cfg, expParameters, 1 , 1);
+
 % This can be used to flush the queue: empty all events that are still present in the queue
 getResponse('flush', cfg, expParameters, 1);
 
