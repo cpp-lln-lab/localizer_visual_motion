2f01f4a99a75baed1db2e083c7ac560a92b7e063
Remi Gau <remi_gau@hotmail.com>
2020-07-25 09:31:57 +0200

    move subfunctions in root folder

---
+++ checkDependencies.m
@@ -0,0 +1,8 @@
+function checkDependencies
+
+    pth = fileparts(mfilename('fullpath'));
+    addpath(fullfile(pth, 'lib', 'CPP_BIDS'));
+    addpath(fullfile(pth, 'lib', 'CPP_PTB'));
+    addpath(fullfile(pth, 'subfun'));
+
+end
