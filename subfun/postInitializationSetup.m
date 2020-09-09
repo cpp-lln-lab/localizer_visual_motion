23199f888fd2b50d089f3af0c84528bdcc21cc0d
Remi Gau <remi_gau@hotmail.com>
2020-09-09 11:37:05 +0200

    create a post-initialization function
    - this is where we set all the parameters that can not be set up till we PTB returns some info about the system

---
+++ subfun/postInitializationSetup.m
@@ -0,0 +1,27 @@
+function varargout = postInitializationSetup(varargin)
+    % varargout = postInitializatinSetup(varargin)
+    
+    % generic function to finalize some set up after psychtoolbox has been
+    % initialized
+    
+    cfg = deal(varargin{:});
+    
+    cfg.dot.matrixWidth = cfg.screen.winWidth;
+    
+    % Convert some values from degrees to pixels
+    cfg.dot = degToPix('size', cfg.dot, cfg);
+    cfg.dot = degToPix('speed', cfg.dot, cfg);
+    
+    % Get dot speeds in pixels per frame
+    cfg.dot.speedPixPerFrame = cfg.dot.speedPix / cfg.screen.monitorRefresh;
+    
+    cfg.aperture = degToPix('xPos', cfg.aperture, cfg);
+    
+    % dots are displayed on a square with a length in visual angle equal to the
+    % field of view
+    cfg.dot.number = round(cfg.dot.density * ...
+        (cfg.dot.matrixWidth / cfg.screen.ppd)^2);
+    
+    varargout = cfg;
+    
+end
