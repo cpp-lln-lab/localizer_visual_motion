98d619eaa0dfb5fb6336c9863943c4ecaf7ced79
marcobarilari <marcobarilr@gmail.com>
2021-07-09 16:37:12 +0200

    rename vars with transparent name

--- subfun/getDirectionBaseVectors.m
+++ subfun/getDirectionBaseVectors.m
@@ -1,20 +1,25 @@
 % (C) Copyright 2020 CPP visual motion localizer developpers
 
-function [CONDITION1_DIRECTIONS, CONDITION2_DIRECTIONS] = getDirectionBaseVectors(cfg)
+function [directionsCondition1, directionsCondition2] = getDirectionBaseVectors(cfg)
 
-    % CONSTANTS
+    % condition1 = 'static';
+    % condition2 = 'motion';
 
     % Set directions for static and motion condition
-    CONDITION1_DIRECTIONS = cfg.design.motionDirections;
-    CONDITION2_DIRECTIONS = repmat(-1, size(CONDITION1_DIRECTIONS)); % static
+    directionsCondition1 = repmat(-1, size(cfg.design.motionDirections));
+    directionsCondition2 = cfg.design.motionDirections;
+
 
     % for for the MT / MST localizer
     if isfield(cfg.design, 'localizer') && strcmpi(cfg.design.localizer, 'MT_MST')
-        CONDITION1_DIRECTIONS = cfg.design.motionDirections;
-        CONDITION2_DIRECTIONS = cfg.design.motionDirections;
+        
+        directionsCondition2 = cfg.design.motionDirections;
+        directionsCondition1 = cfg.design.motionDirections;
 
         if  length(cfg.design.names) == 2
-            CONDITION2_DIRECTIONS = repmat(-1, size(CONDITION1_DIRECTIONS)); % static
+            
+            directionsCondition1 = repmat(-1, size(cfg.design.motionDirections)); % static
+            
         end
 
     end
