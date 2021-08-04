37a3417d8bc888bee6e8245ba2a0d31635550ac6
marcobarilari <marcobarilr@gmail.com>
2021-08-04 18:06:04 +0200

    fix lint

--- subfun/design/setSpeedTargets.m
+++ subfun/design/setSpeedTargets.m
@@ -1,6 +1,7 @@
 function speeds = setSpeedTargets(cfg)
     % If selected as task, it omputes the matrix with the speeds set in
-    % ``cfg.dot.speed`` (computed in pixels per frame) and the target ones (higher = faster, lower = slower).
+    % ``cfg.dot.speed`` (computed in pixels per frame) and the target ones (higher = faster,
+    % lower = slower).
     %
     % If this task is not requeste, the output will be a matrix where the values correspond to the
     % set speed in ``cfg.dot.speed`` (computed in pixels per frame).
