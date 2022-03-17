5158b97d704511b2183a98183899aac97e069a20
Remi Gau <remi_gau@hotmail.com>
2022-03-17 19:28:18 +0100

    add some post init

--- subfun/postInitializationSetup.m
+++ subfun/postInitializationSetup.m
@@ -10,4 +10,17 @@ function cfg = postInitializationSetup(cfg)
 
     cfg = postInitDots(cfg);
 
+    % TODO transfer those if blocks into 'degToPix' (and similarly into pixToDeg)
+    % TODO this kind of generic post initialization can be done systematically at end of initPTB
+    if isfield(cfg.fixation, 'xDisplacement')
+        cfg.fixation = degToPix('xDisplacement', cfg.fixation, cfg);
+    end
+    if isfield(cfg.fixation, 'yDisplacement')
+        cfg.fixation = degToPix('yDisplacement', cfg.fixation, cfg);
+    end
+
+    if isfield(cfg.design, 'xDisplacementAperture')
+        cfg.design = degToPix('xDisplacementAperture', cfg.design, cfg);
+    end
+
 end
